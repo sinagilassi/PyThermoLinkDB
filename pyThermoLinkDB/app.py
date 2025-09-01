e4a7e5a6ecf031041d2bc8928261844bae629caf
sina.gilassi <sina.gilassi@gmail.com>
2025-08-31 23:44:05 -0400

    add multi component methods

--- pyThermoLinkDB/app.py
+++ pyThermoLinkDB/app.py
@@ -5,7 +5,7 @@ from pyThermoDB import ComponentThermoDB, CompBuilder
 from pyThermoDB.models import Component
 # local
 from .docs import ThermoDBHub
-from .models import ComponentModelSource, ComponentThermoDBRules
+from .models import ComponentModelSource, ComponentThermoDBRules, ModelSource
 from .utils import set_component_key, create_rules_from_str, extract_labels_from_rules
 from .config import DEFAULT_RULES_KEY
 
@@ -174,3 +174,72 @@ def build_component_model_source(
     except Exception as e:
         logger.error(f"Error in build_component_model_source: {e}")
         raise Exception(f"Error in build_component_model_source: {e}")
+
+
+def build_components_model_source(
+    components_thermodb: List[ComponentThermoDB],
+    component_key:  Literal[
+        "Name-State",
+        "Formula-State",
+        "Name-Formula-State"
+    ],
+    rules: Optional[
+        Dict[str, ComponentThermoDBRules] | str
+    ] = None,
+) -> ModelSource:
+    '''
+    Build list of component model source from list of component thermodb and rules
+
+    Parameters
+    ----------
+    components_thermodb: List[ComponentThermoDB]
+        List of ComponentThermoDB object containing pythermodb data (`TableData`, `TableEquation`, etc.)
+    component_key: Literal["Name-State", "Formula-State", "Name-Formula-State"]
+        Component key to identify the component in the thermodb hub
+    rules: Optional[Dict[str, ComponentThermoDBRules] | str], optional
+        Rules to map data/equations in the thermodb to the model source.
+
+    Returns
+    -------
+    ModelSource
+        ModelSource object containing data source and equation source for multiple components
+    '''
+    try:
+        # NOTE: create model source for multiple components
+        data_source_all = {}
+        equation_source_all = {}
+        all_check_labels = {}
+        all_label_link = {}
+
+        # iterate over components thermodb
+        for component_thermodb in components_thermodb:
+            # build
+            component_model_source = build_component_model_source(
+                component_thermodb=component_thermodb,
+                component_key=component_key,
+                rules=rules,
+            )
+
+            # NOTE: extract model source
+            # set
+            name_ = set_component_key(
+                component_model_source.component,
+                component_key=component_key
+            )
+            data_source_all[name_] = component_model_source.data_source
+            equation_source_all[name_] = component_model_source.equation_source
+            all_check_labels[name_] = component_model_source.check_labels
+            all_label_link[name_] = component_model_source.label_link
+
+        # NOTE: create model source
+        model_source = ModelSource(
+            data_source=data_source_all,
+            equation_source=equation_source_all,
+            all_check_labels=all_check_labels,
+            all_label_link=all_label_link,
+        )
+
+        return model_source
+    except Exception as e:
+        logger.error(f"Error in build_components_model_source: {e}")
+        raise Exception(f"Error in build_components_model_source: {e}")
