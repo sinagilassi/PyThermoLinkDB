755896b8d36d1e98cd31662d22b7db04505e879e
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-09-16 17:06:33 -0400

    rm component thermodb source

--- pyThermoLinkDB/models/source.py
+++ pyThermoLinkDB/models/source.py
@@ -65,23 +65,3 @@ class ModelSource(BaseModel):
         arbitrary_types_allowed=True,
         extra="allow"
     )
-
-
-class ComponentThermoDBSource(BaseModel):
-    '''
-    ThermoDB source containing component thermodb.
-
-    Attributes
-    ----------
-    component: Component
-        Component thermodb
-    source: str
-        Path to the thermodb file
-    '''
-    component: Component
-    source: str
-
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-        extra="allow"
-    )
