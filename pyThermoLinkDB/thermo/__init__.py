635bd7c7bef77714cb7c419e9b81ce151cdc4572
sina.gilassi <sina.gilassi@gmail.com>
2025-12-06 12:05:27 -0500

    Add mkeq function for creating equation sources and update __all__ in __init__.py

--- pyThermoLinkDB/thermo/__init__.py
+++ pyThermoLinkDB/thermo/__init__.py
@@ -1,12 +1,13 @@
 from .source import Source
 from .equation_source import EquationSource
 from .data_source import DataSource
-from .main import mkeqs, mkdt
+from .main import mkeqs, mkeq, mkdt
 
 __all__ = [
     "Source",
     "EquationSource",
     "DataSource",
     "mkeqs",
+    "mkeq",
     "mkdt",
 ]
--- pyThermoLinkDB/thermo/main.py
+++ pyThermoLinkDB/thermo/main.py
@@ -7,6 +7,7 @@ from pythermodb_settings.utils import set_component_id
 # local
 from ..thermo import Source
 from .equation_sources import EquationSources
+from .equation_source import EquationSource
 from .data_source import DataSource
 
 # NOTE: Logger
@@ -67,6 +68,89 @@ def mkeqs(
         logger.error(f"Error creating equation: {e}")
         return None
 
+
+def mkeq(
+    name: str,
+    component: Component,
+    model_source: ModelSource,
+    component_key: Literal[
+        'Name-State',
+        'Formula-State',
+        'Name',
+        'Formula',
+        'Name-Formula-State',
+        'Formula-Name-State'
+    ] = 'Name-State',
+) -> Optional[EquationSource]:
+    """
+    Make an equation source for a given property and component.
+
+    Parameters
+    ----------
+    name : str
+        The name of the property for which equations are to be calculated.
+    component : Component
+        The chemical component for which properties are to be calculated.
+    model_source : ModelSource
+        The source containing data for calculations.
+    component_key : Literal
+        The key to identify the component in the source data. Defaults to 'Name-State'.
+
+    Returns
+    -------
+    Optional[EquationSource]
+        An EquationSource object if the component equations are found; otherwise, None.
+    """
+    try:
+        # SECTION: Validate inputs
+        if not name:
+            logger.error("Property name must be provided.")
+            return None
+
+        if not isinstance(name, str):
+            logger.error("Property name must be a string.")
+            return None
+
+        # SECTION: Validate inputs
+        if not isinstance(model_source, ModelSource):
+            logger.error("Invalid model_source provided.")
+            return None
+
+        if not isinstance(component, Component):
+            logger.error("Invalid component provided.")
+            return None
+
+        # SECTION: Prepare source
+        Source_ = Source(model_source=model_source)
+
+        # NOTE: component id
+        component_id: str = set_component_id(component, component_key)
+
+        # NOTE: check if property exists in source
+        check_prop = Source_.is_prop_eq_available(
+            component_id=component_id,
+            prop_name=name,
+        )
+
+        # >> check result
+        if not check_prop:
+            logger.error(
+                f"Property '{name}' not found for component '{component_id}' in the source.")
+            return None
+
+        # SECTION: Create XProp object
+        return EquationSource(
+            prop_name=name,
+            component=component,
+            source=Source_,
+            component_key=component_key,
+        )
+    except Exception as e:
+        logger.error(f"Error creating equation: {e}")
+        return None
+
+
+# SECTION: Data Source Maker
 
 def mkdt(
     component: Component,
