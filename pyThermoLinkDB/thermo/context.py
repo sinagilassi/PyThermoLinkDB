d5040ff076c663c73e7bc53e750ffa4f1d707333
sina.gilassi <sina.gilassi@gmail.com>
2026-05-25 00:33:29 -0400

    Enhance Context class: add component_key parameter, refactor pool access, and implement data source retrieval

--- pyThermoLinkDB/thermo/context.py
+++ pyThermoLinkDB/thermo/context.py
@@ -1,38 +1,54 @@
 # import libs
 import logging
 from typing import Optional, Dict, Any
-from pythermodb_settings.models import CustomProperty
+from pythermodb_settings.models import CustomProperty, Component, ComponentKey
+from pythermodb_settings.utils import set_component_id
 # locals
 from ..models.source import ModelSource
+from .source import Source
 
 # NOTE: logger setup
 logger = logging.getLogger(__name__)
 
 
 class Context:
+    """
+    Context class for managing model sources, data sources, equation sources, and pools of constants/variables.
+    """
 
     # NOTE: attributes
-    pools: Dict[str, CustomProperty] = {}
+    _pools: Dict[str, CustomProperty] = {}
 
     # ! required keys
     _required_keys = {'symbol', 'value', 'unit'}
 
     def __init__(
             self,
-            model_source: ModelSource
+            model_source: ModelSource,
+            component_key: ComponentKey
     ):
+        """
+        Context class for managing model sources, data sources, equation sources, and pools of constants/variables.
+
+        Parameters
+        ----------
+        model_source : ModelSource
+            The model source containing data and equations.
+        component_key : ComponentKey
+            The key to identify components in the model source.
+        """
         # NOTE: set
         self._model_source = model_source
 
-        # SECTION: extract
-        # ! data source
-        self._data_source = model_source.data_source
-        # ! equation source
-        self._equation_source = model_source.equation_source
-        # ! data symbol
-        self._data_symbols = model_source.data_symbols
-        # ! equation symbol
-        self._equation_symbols = model_source.equation_symbols
+        # SECTION: source
+        self.source = Source(
+            model_source=model_source,
+            component_key=component_key
+        )
+
+    @property
+    def pools(self) -> Dict[str, CustomProperty]:
+        return self._pools
 
     @property
     def model_source(self) -> ModelSource:
@@ -52,7 +68,7 @@ class Context:
     ) -> bool:
         try:
             # NOTE: check if symbol already exists
-            if symbol in self.pools:
+            if symbol in self._pools:
                 logger.warning(
                     f"Symbol '{symbol}' already exists in pools. Overwriting.")
 
@@ -83,7 +99,7 @@ class Context:
                     f"Error creating CustomProperty for symbol '{symbol}': {e}")
                 return False
 
-            self.pools[symbol] = custom_property
+            self._pools[symbol] = custom_property
             logger.info(f"Added symbol '{symbol}' to pools.")
             return True
         except Exception as e:
@@ -108,19 +124,54 @@ class Context:
             return False
 
     # NOTE: get from pool
-
+    # ! get from pool by symbol
     def get_from_pool(
             self,
             symbol: str
     ) -> Optional[CustomProperty]:
         try:
-            if symbol in self.pools:
-                return self.pools[symbol]
+            if symbol in self._pools:
+                return self._pools[symbol]
             else:
                 logger.warning(f"Symbol '{symbol}' not found in pools.")
                 return None
         except Exception as e:
             logger.error(f"Error retrieving symbol '{symbol}' from pools: {e}")
+            return None
+
+    # ! get from data source by symbol & component id, then from pool if not found
+
+    def gt(
+            self,
+            symbol: str,
+            component: Component,
+            component_key: ComponentKey
+    ) -> Optional[CustomProperty]:
+        try:
+            # NOTE: try to get from data source
+            if self.source is not None:
+                data_source = self.source.datasource
+                if data_source is not None:
+                    # >> component id
+                    component_id = set_component_id(component, component_key)
+
+                    # res
+                    result = self.source.component_data_extractor(
+                        component_id=component_id,
+                    )
+
+                    # >> check result
+                    if result is not None:
+                        return CustomProperty(
+                            symbol=symbol,
+                            value=result['value'],
+                            unit=result['unit']
+                        )
+
+            # NOTE: if not found in data source, try to get from pool
+            return self.get_from_pool(symbol)
+        except Exception as e:
+            logger.error(f"Error getting symbol '{symbol}': {e}")
             return None
 
     # NOTE: remove from pool
@@ -130,8 +181,8 @@ class Context:
             symbol: str
     ) -> bool:
         try:
-            if symbol in self.pools:
-                del self.pools[symbol]
+            if symbol in self._pools:
+                del self._pools[symbol]
                 logger.info(f"Removed symbol '{symbol}' from pools.")
                 return True
             else:
@@ -145,7 +196,7 @@ class Context:
 
     def clear_pool(self) -> bool:
         try:
-            self.pools.clear()
+            self._pools.clear()
             logger.info("Cleared all symbols from pools.")
             return True
         except Exception as e:
