65f3ecabffb81e37debdf2cd72776985fa3047f6
sina.gilassi <sina.gilassi@gmail.com>
2026-06-09 00:16:55 -0400

    Add build_constants_model_source to __init__.py exports

--- pyThermoLinkDB/__init__.py
+++ pyThermoLinkDB/__init__.py
@@ -6,6 +6,7 @@ from .app import (
     init,
     build_component_model_source,
     build_components_model_source,
+    build_constants_model_source,
     build_model_source,
     load_and_build_model_source,
     load_and_build_component_model_source,
@@ -25,6 +26,7 @@ from .thermo import (
 )
 
 __all__ = [
+    # config
     "__author__",
     "__version__",
     "__description__",
@@ -32,6 +34,7 @@ __all__ = [
     "init",
     "build_component_model_source",
     "build_components_model_source",
+    "build_constants_model_source",
     "build_model_source",
     "load_and_build_model_source",
     "build_mixture_model_source",
