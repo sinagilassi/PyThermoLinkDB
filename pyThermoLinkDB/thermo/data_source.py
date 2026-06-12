62d5b185497cd045ac19adf8d71993b731ab447a
sina.gilassi <sina.gilassi@gmail.com>
2026-06-12 01:55:17 -0400

    Remove unused import of Literal from typing in data_source.py

--- pyThermoLinkDB/thermo/data_source.py
+++ pyThermoLinkDB/thermo/data_source.py
@@ -1,6 +1,6 @@
 # import libs
 import logging
-from typing import Dict, Literal, Any, Optional, List
+from typing import Dict, Any, Optional, List
 from pythermodb_settings.models import Component, ComponentKey
 from pythermodb_settings.utils import set_component_id
 # local
