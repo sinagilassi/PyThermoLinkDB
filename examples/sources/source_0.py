cfd91df8f7e7a20d75f632d12143498820b936e0
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-06-12 22:01:12 -0400

    Add example scripts for building thermodynamic databases and extracting component data

---
+++ examples/sources/source_0.py
@@ -0,0 +1,249 @@
+# import packages/modules
+import os
+from rich import print
+from typing import Callable, Dict, Optional, Union, List, Any
+import pyThermoDB as ptdb
+
+from pythermodb_settings.models import Component, Pressure, Temperature, CustomProp, Volume, CustomProperty, ComponentRule
+from pyThermoDB import ComponentThermoDB, ConstantsThermoDB
+from pyThermoDB import (
+    build_component_thermodb_from_reference,
+    build_constants_thermodb_from_reference
+)
+# locals
+import pyThermoLinkDB as ptdblink
+from pyThermoLinkDB import (
+    build_component_model_source,
+    build_components_model_source,
+    build_constants_model_source,
+    build_model_source
+)
+from pyThermoLinkDB.models import ComponentModelSource, ModelSource, ConstantsModelSource
+from pyThermoLinkDB.thermo import Source
+from examples.reference_2 import REFERENCE_CONTENT
+
+# ! version
+print(ptdb.__version__)
+print(ptdblink.__version__)
+
+# ====================================================
+# SECTION: BUILD COMPONENT THERMODB
+# ====================================================
+# NOTE: parent directory
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+print(parent_dir)
+
+# NOTE: thermodb directory
+thermodb_dir = os.path.join(parent_dir, 'thermodb')
+print(thermodb_dir)
+
+# NOTE: create component
+# ! propane
+# carbon dioxide
+CO2 = Component(
+    name='carbon dioxide',
+    formula='CO2',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 1,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# Hydrogen
+H2 = Component(
+    name='hydrogen',
+    formula='H2',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 3,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# methanol
+CH3OH = Component(
+    name='methanol',
+    formula='CH3OH',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 0,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# ethanol
+C2H5OH = Component(
+    name='ethanol',
+    formula='C2H5OH',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 0,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# water
+H2O = Component(
+    name='water',
+    formula='H2O',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 0,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# Carbon monoxide
+CO = Component(
+    name='carbon monoxide',
+    formula='CO',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 0,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# ethylene
+C2H4 = Component(
+    name='ethylene',
+    formula='C2H4',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 1,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# ethane
+C2H6 = Component(
+    name='ethane',
+    formula='C2H6',
+    state='g',
+    X={
+        "name": "mole",
+        "value": 0,
+        "unit": "mol",
+        "symbol": "n"
+    }
+)
+
+# components
+components = [CO2, C2H5OH]
+
+# NOTE: ignore state properties
+ignore_state_props = ['MW', 'VaPr', 'Cp_IG', 'Cp_LIQ']
+
+# ====================================================
+# SECTION: build components thermodb
+# ====================================================
+thermodb_components: List[ComponentThermoDB] = []
+
+for comp in components:
+    thermodb_component: ComponentThermoDB | None = build_component_thermodb_from_reference(
+        component_name=comp.name,
+        component_formula=comp.formula,
+        component_state=comp.state,
+        reference_content=REFERENCE_CONTENT,
+        ignore_state_props=ignore_state_props,
+    )
+    if thermodb_component is None:
+        raise ValueError(f"thermodb_component for {comp.name} is None")
+
+    # log
+    print(thermodb_component)
+    thermodb_components.append(thermodb_component)
+
+
+# NOTE: with partially matched rules
+component_model_source: List[ComponentModelSource] = build_components_model_source(
+    components_thermodb=thermodb_components,
+    rules=None,
+)
+print(component_model_source)
+
+# ====================================================
+# SECTION: build constants thermodb
+# ====================================================
+# NOTE: build constants thermodb from reference
+constants_thermodb: ConstantsThermoDB | None = build_constants_thermodb_from_reference(
+    reference_content=REFERENCE_CONTENT,
+)
+# >> check
+if constants_thermodb is None:
+    raise ValueError("constants_thermodb is None")
+# log
+print(constants_thermodb)
+
+# NOTE: build model source with constants thermodb
+# ! no rules
+constants_model_source: ConstantsModelSource = build_constants_model_source(
+    constants_thermodb=constants_thermodb,
+    rules=None,
+)
+# >> log
+print(constants_model_source)
+
+
+# ====================================================
+# SECTION: build model source
+# ====================================================
+# NOTE: all model source
+# ! with rules
+sources: list = [constants_model_source] + component_model_source
+
+# model source
+model_source: ModelSource = build_model_source(
+    source=sources,
+)
+# >> log
+print(model_source)
+
+# ====================================================
+# SECTION: THERMODB LINK CONFIGURATION
+# ====================================================
+
+# build datasource & equationsource
+datasource = model_source.data_source
+equationsource = model_source.equation_source
+constantssource = model_source.constants_source
+# symbols
+data_symbols = model_source.data_symbols
+equation_symbols = model_source.equation_symbols
+constants_symbols = model_source.constants_symbols
+
+# =======================================
+# ✅ MAKE SOURCE
+# =======================================
+# component key
+component_key = 'Formula-State'
+# init source
+source = Source(
+    model_source=model_source,
+    component_key=component_key
+)
+print(source)
+
+# make equation source core
+eq_src = source.eq_builder(
+    components=[CO2],
+    prop_name='Cp_IG',
+    # component_key='Name-Formula',
+    component_keys=['Name-State', 'Formula-State', 'Name-Formula-State']
+)
+print(eq_src)
---
+++ examples/sources/source_1.py
@@ -0,0 +1,78 @@
+# import packages/modules
+import os
+from rich import print
+from typing import Callable, Dict, Optional, Union, List, Any
+import pyThermoDB as ptdb
+# locals
+import pyThermoLinkDB as ptdblink
+# ! source
+from pyThermoLinkDB.thermo import Source
+# ! model source
+from examples.model_source_2 import model_source, CO2
+
+# ! version
+print(ptdb.__version__)
+print(ptdblink.__version__)
+
+# ====================================================
+# SECTION: BUILD COMPONENT THERMODB
+# ====================================================
+# NOTE: parent directory
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+print(parent_dir)
+
+# NOTE: thermodb directory
+thermodb_dir = os.path.join(parent_dir, 'thermodb')
+print(thermodb_dir)
+
+# ====================================================
+# SECTION: THERMODB LINK CONFIGURATION
+# ====================================================
+
+# build datasource & equationsource
+datasource = model_source.data_source
+equationsource = model_source.equation_source
+constantssource = model_source.constants_source
+# symbols
+data_symbols = model_source.data_symbols
+equation_symbols = model_source.equation_symbols
+constants_symbols = model_source.constants_symbols
+
+# =======================================
+# SECTION: ✅ MAKE SOURCE
+# =======================================
+# component key
+component_key = 'Formula-State'
+# init source
+source = Source(
+    model_source=model_source,
+    component_key=component_key
+)
+print(source)
+
+# NOTE: make equation source core
+eq_src = source.eq_builder(
+    components=[CO2],
+    prop_name='Cp_IG',
+    # component_key='Name-Formula',
+    component_keys=['Name-State', 'Formula-State', 'Name-Formula-State']
+)
+print(eq_src)
+
+# NOTE: get component data
+comp_data = source.component_data_extractor('CO2-g')
+print("Component data for")
+print(comp_data)
+
+# specific data
+comp_data_specific = source.data_extractor('CO2-g', 'EnFo_IG')
+print("Component data for")
+print(comp_data_specific)
+
+# NOTE: access to constants source
+# ! check availability of constant 'R'
+check_0 = source.is_constant_available('R')
+print(f"Is constant 'R' available? {check_0}")
+# ! get constant 'R'
+const_0 = source.constants_extractor('R')
+print(const_0)
