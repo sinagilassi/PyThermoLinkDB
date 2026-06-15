61b8e094797a10b77232c660e8ad86ce9e281e88
sina.gilassi <sina.gilassi@gmail.com>
2026-06-15 01:24:09 -0400

    Refactor imports in constants_source.py to include Tuple for improved type handling

--- pyThermoLinkDB/thermo/constants_source.py
+++ pyThermoLinkDB/thermo/constants_source.py
@@ -1,8 +1,6 @@
 # import libs
 import logging
-from typing import Dict, Literal, Any, Optional, List
-from pythermodb_settings.models import Component, ComponentKey
-from pythermodb_settings.utils import set_component_id
+from typing import Dict, Literal, Any, Optional, List, Tuple
 # local
 from ..thermo import Source
 from ..models.component_models import PropResult
