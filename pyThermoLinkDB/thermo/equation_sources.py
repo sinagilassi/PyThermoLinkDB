f6518f1a7e9b0f2006e8769373077538e9610cac
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-06-14 23:03:31 -0400

    Enhance EquationSourcesCore to support selective equation building and input validation, adding properties for input sources and symbols.

--- pyThermoLinkDB/thermo/equation_sources.py
+++ pyThermoLinkDB/thermo/equation_sources.py
@@ -1,12 +1,17 @@
 # import libs
 import logging
-from typing import Dict, Literal, Any, Optional, List, cast
+from typing import Dict, Literal, Any, Optional, List, cast, Tuple
 from pythermodb_settings.models import Component, ComponentKey
 from pyThermoDB.core import TableEquation
 from pythermodb_settings.utils import set_component_id
 # local
 from ..thermo import Source
 from .equation_source import EquationSourceCore
+from ..utils.input_builder import (
+    UnitAvailabilityFn,
+    UnitConversionFn,
+    validate_inputs_availability_and_units
+)
 
 # NOTE: Logger
 logger = logging.getLogger(__name__)
@@ -53,6 +58,7 @@ class EquationSourcesCore:
         source: Source,
         component_key: ComponentKey = 'Name-State',
         build_all: bool = False,
+        build_list: Optional[list[str]] = None,
     ) -> None:
         """
         Initialize EquationSource with a component and source.
@@ -71,6 +77,8 @@ class EquationSourcesCore:
             The key to identify the component in the source data. Defaults to 'Name-State'.
         build_all : bool
             Whether to build all available equations for the component. Defaults to False.
+        build_list : Optional[list[str]]
+            A list of specific equation names to build. If provided, only these equations will be built. Defaults to None.
         """
         # NOTE: component
         self.component = component
@@ -78,6 +86,10 @@ class EquationSourcesCore:
         self.source = source
         # NOTE: component key
         self.component_key = component_key
+        # NOTE: build all
+        self.build_all = build_all
+        # NOTE: build list
+        self.build_list = build_list
 
         # SECTION: set component id
         self.component_id = set_component_id(
@@ -97,12 +109,18 @@ class EquationSourcesCore:
 
         # SECTION: build all equations if requested
         if (
-            build_all is True and
+            self.build_all is True and
             self.component_equations is not None
         ):
+            # ! build all sources and store in _src
             self._src = self.build()
+            # ! build inputs sources for all equations and store in _inputs_src
+            self._inputs_src, self._inputs_symbols_src = self.build_inputs_src()
         else:
+            # ! initialize empty sources
             self._src = {}
+            self._inputs_src = {}
+            self._inputs_symbols_src = {}
 
     # SECTION: properties
     @property
@@ -116,6 +134,30 @@ class EquationSourcesCore:
             The source dictionary.
         """
         return self._src
+
+    @property
+    def inputs_src(self) -> Dict[str, Optional[Dict[str, Any]]]:
+        """
+        Get the source dictionary of all equation inputs.
+
+        Returns
+        -------
+        Dict[str, Optional[Dict[str, Any]]]
+            The source dictionary of all equation inputs.
+        """
+        return self._inputs_src
+
+    @property
+    def inputs_symbols_src(self) -> Dict[str, Optional[List[str]]]:
+        """
+        Get the source dictionary of all equation input symbols.
+
+        Returns
+        -------
+        Dict[str, Optional[List[str]]]
+            The source dictionary of all equation input symbols.
+        """
+        return self._inputs_symbols_src
 
     # SECTION: list available equations
 
@@ -195,8 +237,17 @@ class EquationSourcesCore:
                 logger.error("Component equations are not available.")
                 return eq_sources
 
+            # NOTE: if build_list is provided, filter equations to build
+            if self.build_list is not None:
+                component_equations_ = {
+                    k: v for k, v in self.component_equations.items() if k in self.build_list
+                }
+            else:
+                component_equations_ = self.component_equations
+
             # NOTE: loop through available equations and create sources
-            for eq_id in self.component_equations.keys():
+            for eq_id in component_equations_.keys():
+                # set source for the equation
                 eq_sources[eq_id] = self.eq(name=eq_id)
 
             return eq_sources
@@ -238,3 +289,138 @@ class EquationSourcesCore:
         except Exception as e:
             logger.error(f"Error selecting equation: {e}")
             return None
+
+    # SECTION: equation inputs source builder
+    def build_inputs_src(
+            self
+    ) -> Tuple[Dict[str, Optional[Dict[str, Any]]], Dict[str, Optional[List[str]]]]:
+        """
+        Build the source of inputs for all equations available for the component.
+
+        Returns
+        -------
+        Tuple[Dict[str, Optional[Dict[str, Any]]], Dict[str, Optional[List[str]]]]
+            A tuple containing:
+                - A dictionary of equation IDs to their corresponding input definitions, where the value is None if the inputs could not be built.
+                - A dictionary of equation IDs to their corresponding list of input symbols, where the value is None if the inputs could not be built.
+        """
+        try:
+            # NOTE: build inputs sources for all available equations
+            # ! full inputs source
+            inputs_src: Dict[str, Optional[Dict[str, Any]]] = {}
+            # ! input symbols source
+            inputs_symbols_src: Dict[str, Optional[List[str]]] = {}
+
+            # >> check if component equations are available
+            if self._src is None:
+                logger.error("Equation sources have not been built.")
+                return inputs_src, inputs_symbols_src
+
+            # NOTE: loop through available equations and create sources
+            for eq_id, eq_source in self._src.items():
+                if eq_source is not None:
+                    inputs_src[eq_id] = eq_source.inputs
+                    inputs_symbols_src[eq_id] = list(eq_source.arg_identifiers)
+                else:
+                    inputs_src[eq_id] = None
+                    inputs_symbols_src[eq_id] = None
+
+            return inputs_src, inputs_symbols_src
+        except Exception as e:
+            logger.error(f"Error building inputs sources: {e}")
+            return {}, {}
+
+    # SECTION: Check inputs availability and units for a specific equation source
+    def validate_inputs_availability_and_units(
+            self,
+            eq_symbol: str,
+            inputs: Dict[str, Any],
+            unit_availability_fn: UnitAvailabilityFn
+    ) -> Tuple[bool, Optional[Dict[str, bool]], bool, Optional[Dict[str, bool]]]:
+        """
+        Validate the availability of required inputs and their units for a specific equation source.
+
+        Parameters
+        ----------
+        eq_symbol : str
+            The ID of the equation for which to validate inputs.
+        inputs : Dict[str, Any]
+            A dictionary of input values provided at runtime, where keys are input symbols and values are the corresponding input values.
+        unit_conversion_fn : UnitConversionFn
+            A function that takes an input value, its current unit, and a target unit, and returns the converted value.
+        unit_availability_fn : UnitAvailabilityFn
+            A function that takes a unit string and returns a boolean indicating whether the unit is available in the system.
+
+        Returns
+        -------
+        Tuple[bool, Optional[Dict[str, bool]], Optional[Dict[str, bool]]]
+            A tuple containing:
+                - A boolean indicating whether all required inputs are available and have valid units.
+                - A dictionary of validated and converted input values, where keys are input symbols and values are the corresponding validated and converted input values. This is None if validation fails.
+                - A boolean indicating whether all required units are available.
+                - A dictionary of input symbols to their expected units, where keys are input symbols and values are the expected unit strings. This is None if validation fails.
+        """
+        try:
+            # NOTE: check if equation source is available
+            eq_source = self._src.get(eq_symbol)
+
+            # >> check if equation source is available
+            if eq_source is None:
+                logger.error(
+                    f"Equation source for symbol '{eq_symbol}' is not available.")
+                return False, None, False, None
+
+            # NOTE: validate inputs availability and units using the utility function
+            return validate_inputs_availability_and_units(
+                eq_inputs=eq_source.inputs,
+                inputs=inputs,
+                unit_availability_fn=unit_availability_fn
+            )
+        except Exception as e:
+            logger.error(f"Error validating inputs: {e}")
+            return False, None, False, None
+
+    # NOTE: validate all
+    def validate_all_inputs(
+            self,
+            inputs: Dict[str, Any],
+            unit_availability_fn: UnitAvailabilityFn
+    ):
+        """
+        Validate all equation sources.
+
+        Parameters
+        ----------
+        inputs : Dict[str, Any]
+            A dictionary of input values provided at runtime, where keys are input symbols and values are the corresponding input values.
+        unit_availability_fn : UnitAvailabilityFn
+            A function that takes a unit string and returns a boolean indicating whether the unit is available in the system.
+
+        Returns
+        -------
+        Tuple[bool, Dict[str, bool]]
+            A tuple containing:
+                - A boolean indicating whether all inputs for all equations are available and have valid units.
+                - A dictionary mapping each equation ID to a boolean indicating whether its inputs are valid.
+        """
+        try:
+            # NOTE: validate all equation sources
+            validation_results = {}
+
+            # iterate through all equation sources and validate their inputs
+            for eq_id in self._src.keys():
+                valid_inputs_, _, valid_units_, _ = self.validate_inputs_availability_and_units(
+                    eq_symbol=eq_id,
+                    inputs=inputs,
+                    unit_availability_fn=unit_availability_fn
+                )
+                validation_results[eq_id] = {
+                    "inputs": valid_inputs_,
+                    "units": valid_units_,
+                    "overall": valid_inputs_ and valid_units_
+                }
+
+            return validation_results
+        except Exception as e:
+            logger.error(f"Error validating all inputs: {e}")
+            return False, {}
