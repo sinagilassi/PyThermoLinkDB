5cceb668f6563485500e710e67a39f59e56ed3eb
sina.gilassi <sina.gilassi@gmail.com>
2026-06-16 17:04:23 -0400

    Add ThermoUtils class for thermodynamic model operations

---
+++ pyThermoLinkDB/builders/thermo_utils.py
@@ -0,0 +1,17 @@
+# import libs
+import logging
+from typing import List, Optional
+from pythermodb_settings.models import Component, ComponentKey
+from pythermodb_settings.utils import generate_component_references
+# locals
+
+# NOTE: logger setup
+logger = logging.getLogger(__name__)
+
+
+class ThermoUtils:
+    """
+    Utility class for thermodynamic model operations.
+    """
+
+    pass
