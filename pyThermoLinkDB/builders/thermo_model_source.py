e1af5f8d15ae6f778572500a464c06c9b8141c09
sina.gilassi <sina.gilassi@gmail.com>
2026-06-16 17:05:22 -0400

    Remove unused import of generate_component_references from pythermodb_settings.utils

--- pyThermoLinkDB/builders/thermo_model_source.py
+++ pyThermoLinkDB/builders/thermo_model_source.py
@@ -2,7 +2,6 @@
 import logging
 from typing import List, Optional, Dict, Any
 from pythermodb_settings.models import Component, ComponentKey
-from pythermodb_settings.utils import generate_component_references
 # locals
 
 
