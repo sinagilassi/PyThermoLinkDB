c81182bdc0c65947e184c6f98b398d19ddb6cbd0
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-06-17 17:25:07 -0400

    Refactor config_attributes method to improve handling of thermo data, equations, and constants

--- pyThermoLinkDB/builders/thermo_model_source.py
+++ pyThermoLinkDB/builders/thermo_model_source.py
@@ -216,20 +216,18 @@ class ThermoModelSource:
         Configure the attributes of the thermodynamic model source.
         """
         # NOTE: currently, there are no additional attributes to configure.
+        component_ids = self.component_references.get('component_ids', [])
 
         # ! data variables
-        # >>> init res
-        dt_value = np.array([])
-        dt_comp = {}
-        dt_src: Dict[str, CustomProperty] = {}
-
         # iterate over thermo data and set attributes
         for symbol in self.thermo_data:
             # > extract property data for the symbol for all components
-            dt_src = {}
+            dt_value: List[float] = []
+            dt_comp: Dict[str, float] = {}
+            dt_src: Dict[str, CustomProperty] = {}
 
             # iterate over components and extract data source for the symbol
-            for comp_id in self.component_references['component_ids']:
+            for comp_id in component_ids:
                 res_dt: DataSourceCore | None = self.thermo_data_source.get(
                     comp_id, None
                 )
@@ -237,9 +235,14 @@ class ThermoModelSource:
                 # >> check
                 if res_dt is not None:
                     res_dt_: CustomProperty | None = res_dt.select(
-                        symbol=symbol)
+                        symbol=symbol
+                    )
+
+                    # >> check
                     if res_dt_ is not None:
                         dt_src[comp_id] = res_dt_
+                        dt_comp[comp_id] = float(res_dt_.value)
+                        dt_value.append(float(res_dt_.value))
                     else:
                         logger.warning(
                             f"Data source for symbol '{symbol}' not found for component '{comp_id}'."
@@ -251,22 +254,27 @@ class ThermoModelSource:
             # ? set attributes for each symbol
             setattr(self, f"{symbol}_src", dt_src)
             setattr(self, f"{symbol}_comp", dt_comp)
-            setattr(self, f"{symbol}_value", dt_value)
+            setattr(self, f"{symbol}_value", np.array(dt_value))
 
         # ! equation variables
-        # >>> init res
-        eqn_value = np.array([])
-        eqn_comp: Dict[str, float] = {}
-        eqn_src: Dict[str, EquationSourceCore] = {}
-
         # iterate over thermo equations and set attributes
         for symbol in self.thermo_equations:
             # > extract equation data for the symbol for all components
+            eqn_value: Optional[List[float]] = None
+            eqn_comp: Optional[Dict[str, float]] = None
             eqn_src: Dict[str, EquationSourceCore] = {}
 
             # iterate over components and extract equation source for the symbol
-            for comp_id in self.component_references['component_ids']:
-                eqs_comp: EquationSourcesCore = self.thermo_equations_source[comp_id]
+            for comp_id in component_ids:
+                eqs_comp: EquationSourcesCore | None = self.thermo_equations_source.get(
+                    comp_id, None
+                )
+                if eqs_comp is None:
+                    logger.warning(
+                        f"Equation source for symbol '{symbol}' not found for component '{comp_id}'."
+                    )
+                    continue
+
                 eqn_src_comp: EquationSourceCore | None = eqs_comp.select(
                     name=symbol
                 )
@@ -284,10 +292,6 @@ class ThermoModelSource:
             setattr(self, f"{symbol}_value", eqn_value)
 
         # ! constants variables
-        # >>> init res
-        const_value = np.array([])
-        const_src: ConstantResult | None = None
-
         # iterate over thermo constants and set attributes
         for symbol in self.thermo_constants:
             # source
@@ -308,8 +312,11 @@ class ThermoModelSource:
             # >> check if constant source for the symbol was found
             if const_src is not None:
                 setattr(self, f"{symbol}_src", const_src)
-                setattr(self, f"{symbol}_value", const_value)
+                setattr(self, f"{symbol}_value", const_src.value)
             else:
                 logger.warning(
                     f"Constant source for symbol '{symbol}' not found in the model source."
                 )
+                # set attributes to None if not found
+                setattr(self, f"{symbol}_src", None)
+                setattr(self, f"{symbol}_value", None)
