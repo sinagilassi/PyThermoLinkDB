31da65b3d4b33ae432b9e09f2858fcc1e373b7d3
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-06-18 18:47:09 -0400

    Refactor imports in __init__.py to improve organization and clarity

--- pyThermoLinkDB/builders/__init__.py
+++ pyThermoLinkDB/builders/__init__.py
@@ -1,15 +1,24 @@
-# main
-from .main import build_thermo_model_source, build_custom_model_source
-
 # thermo model source
 from .thermo_model_source import ThermoModelSource
 
 # custom model source
 from .thermo_custom_source import ThermoCustomSource
 
+# thermo source
+from .thermo_source import ThermoSource
+
+# main
+from .main import (
+    build_thermo_model_source,
+    build_custom_model_source,
+    build_thermo_source
+)
+
 __all__ = [
     "build_thermo_model_source",
     "build_custom_model_source",
+    "build_thermo_source",
     "ThermoModelSource",
     "ThermoCustomSource",
+    "ThermoSource",
 ]
