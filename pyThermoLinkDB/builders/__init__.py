fff9eff1f9f2df0cffbd5019a20d6d1835b5ac13
sina.gilassi <sina.gilassi@gmail.com>
2026-06-18 12:00:24 -0400

    Replace CustomModelSource with ThermoCustomSource in __init__.py

--- pyThermoLinkDB/builders/__init__.py
+++ pyThermoLinkDB/builders/__init__.py
@@ -5,11 +5,11 @@ from .main import build_thermo_model_source, build_custom_model_source
 from .thermo_model_source import ThermoModelSource
 
 # custom model source
-from .custom_model_source import CustomModelSource
+from .thermo_custom_source import ThermoCustomSource
 
 __all__ = [
     "build_thermo_model_source",
     "build_custom_model_source",
     "ThermoModelSource",
-    "CustomModelSource",
+    "ThermoCustomSource",
 ]
