e788b85194e62210113ba51c7e98dd05af2337f1
sina.gilassi <sina.gilassi@gmail.com>
2026-06-17 22:30:40 -0400

    Add ThermoModelSource to exports in builders module

--- pyThermoLinkDB/builders/__init__.py
+++ pyThermoLinkDB/builders/__init__.py
@@ -1,6 +1,10 @@
 # main
 from .main import build_thermo_model_source
 
+# thermo model source
+from .thermo_model_source import ThermoModelSource
+
 __all__ = [
-    "build_thermo_model_source"
+    "build_thermo_model_source",
+    "ThermoModelSource",
 ]
