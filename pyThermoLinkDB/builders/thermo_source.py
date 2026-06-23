663f1d65d8d95fdd322c92c588d0daaff74e928c
sina.gilassi <sina.gilassi@gmail.com>
2026-06-23 13:23:40 -0400

    Refactor ThermoSource: enhance type hinting for thermo_source and configuration methods

--- pyThermoLinkDB/builders/thermo_source.py
+++ pyThermoLinkDB/builders/thermo_source.py
@@ -1,6 +1,6 @@
 """Container for built model and custom thermodynamic sources."""
 
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 
 from pythermodb_settings.models import Component, ComponentKey
 
@@ -19,8 +19,34 @@ class ThermoSource:
             thermo_custom_source: Optional[ThermoCustomSource],
             description: Optional[str] = None,
     ) -> None:
+        # NOTE: set attributes
         self.components = components
         self.component_key = component_key
         self.thermo_model_source = thermo_model_source
         self.thermo_custom_source = thermo_custom_source
         self.description = description
+
+        # NOTE: thermo source
+        self._thermo_source: Dict[str, Dict[str, Any]] = {
+            'model_source': {},
+            'custom_source': {},
+        }
+
+    # SECTION: Properties
+    @property
+    def thermo_source(self) -> Dict[str, Dict[str, Any]]:
+        """Return thermo source dictionary."""
+        return self._thermo_source
+
+    # SECTION: Thermo source configuration
+    def _configure_thermo_source(self) -> None:
+        """
+        Configure thermo source dictionary.
+        """
+        # ! model source
+        if self.thermo_model_source is not None:
+            self._thermo_source["model_source"] = self.thermo_model_source.thermo_src
+
+        # ! custom source
+        if self.thermo_custom_source is not None:
+            self._thermo_source["custom_source"] = self.thermo_custom_source.thermo_src
