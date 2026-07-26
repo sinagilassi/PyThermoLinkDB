8f8ec811c9e97e1629c66b829a02e762c816c90e
sina.gilassi <sina.gilassi@gmail.com>
2026-07-26 18:30:54 -0400

    Add MatrixDataSourceCore and additional exports to __all__ for improved module accessibility

--- pyThermoLinkDB/thermo/__init__.py
+++ pyThermoLinkDB/thermo/__init__.py
@@ -1,15 +1,17 @@
 from .source import Source
 from .equation_source import EquationSourceCore
 from .data_source import DataSourceCore
+from .matrixdata_source import MatrixDataSourceCore
 from .constants_source import ConstantsSourceCore
 from .equation_sources import EquationSourcesCore
-from .main import mkeqs, mkeqss, mkeq, mkdt, mkdts, mkct
+from .main import mkeqs, mkeqss, mkeq, mkdt, mkdts, mkct, mkmdt, mkmdts
 from .context import Context
 
 __all__ = [
     "Source",
     "EquationSourceCore",
     "DataSourceCore",
+    "MatrixDataSourceCore",
     "ConstantsSourceCore",
     "EquationSourcesCore",
     "mkeqs",
@@ -18,5 +20,7 @@ __all__ = [
     "mkdt",
     "mkdts",
     "mkct",
+    "mkmdt",
+    "mkmdts",
     "Context"
 ]
