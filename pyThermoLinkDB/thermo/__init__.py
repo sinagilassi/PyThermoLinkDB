411108d9ffd19047ab6e0e938c368b07914c8483
sina.gilassi <sina.gilassi@gmail.com>
2026-08-08 21:29:21 -0400

    Fix import statement for MatrixDataSourceCore by correcting the module name

--- pyThermoLinkDB/thermo/__init__.py
+++ pyThermoLinkDB/thermo/__init__.py
@@ -1,7 +1,7 @@
 from .source import Source
 from .equation_source import EquationSourceCore
 from .data_source import DataSourceCore
-from .matrixdata_source import MatrixDataSourceCore
+from .matrix_data_source import MatrixDataSourceCore
 from .constants_source import ConstantsSourceCore
 from .equation_sources import EquationSourcesCore
 from .main import mkeqs, mkeqss, mkeq, mkdt, mkdts, mkct, mkmdt, mkmdts
