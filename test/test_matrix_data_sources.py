945a4d9ed4e7e7675d869b974905362952ecfc11
sina.gilassi <sina.gilassi@gmail.com>
2026-08-08 22:30:58 -0400

    Add test for mkmdts function to validate MatrixDataSourcesCore instantiation

--- test/test_matrix_data_sources.py
+++ test/test_matrix_data_sources.py
@@ -1,5 +1,7 @@
 from pythermodb_settings.models import Component
 
+from pyThermoLinkDB.models import ModelSource
+from pyThermoLinkDB.thermo.main import mkmdts
 from pyThermoLinkDB.thermo.matrix_data_sources import MatrixDataSourcesCore
 
 
@@ -77,3 +79,38 @@ def test_matrix_data_sources_reports_failed_property(monkeypatch):
     }
     assert matrix_sources.build_status() is False
     assert matrix_sources.select("unknown") is None
+
+
+def test_mkmdts_returns_matrix_data_sources_core_by_mixture_id(monkeypatch):
+    built_args = []
+
+    class FakeMatrixDataSourcesCore:
+        def __init__(self, **kwargs):
+            built_args.append(kwargs)
+            self.mixture_ids = ["ethanol|methanol"]
+
+    monkeypatch.setattr(
+        "pyThermoLinkDB.thermo.main.MatrixDataSourcesCore",
+        FakeMatrixDataSourcesCore,
+    )
+    methanol = Component(name="methanol", formula="CH3OH", state="l")
+    ethanol = Component(name="ethanol", formula="C2H5OH", state="l")
+    model_source = ModelSource(data_source={}, equation_source={})
+
+    matrix_sources = mkmdts(
+        mixture_components=[[methanol, ethanol]],
+        model_source=model_source,
+        mixture_key="Name",
+        extract_list=["alpha"],
+        check_build=True,
+    )
+
+    assert matrix_sources is not None
+    assert list(matrix_sources) == ["ethanol|methanol"]
+    assert isinstance(
+        matrix_sources["ethanol|methanol"],
+        FakeMatrixDataSourcesCore,
+    )
+    assert built_args[0]["mixture_components"] == [[methanol, ethanol]]
+    assert built_args[0]["extract_list"] == ["alpha"]
+    assert built_args[0]["check_build"] is True
