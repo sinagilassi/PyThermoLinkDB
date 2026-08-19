26480fe58404c115ccbd405e0822b53d665a0550
sina.gilassi <sina.gilassi@gmail.com>
2026-08-18 23:17:41 -0400

    add hub_tools functions to __all__ exports in utils module

--- pyThermoLinkDB/utils/__init__.py
+++ pyThermoLinkDB/utils/__init__.py
@@ -13,6 +13,12 @@ from .properties import (
 )
 from .loader import create_rules_from_str
 from .mixture_tools import canonicalize_mixture_name
+from .hub_tools import (
+    ensure_thermo_source_hub_config,
+    thermo_source_hub_config_from_json,
+    thermo_source_hub_config_from_str,
+    thermo_source_hub_config_from_yaml,
+)
 # protocols
 from .unit_tools import UnitAvailabilityFn, UnitConversionFn
 
@@ -29,6 +35,10 @@ __all__ = [
     "look_up_default_rules",
     "combine_rules_into_constants_key",
     "extract_labels_from_constants_rules",
+    "ensure_thermo_source_hub_config",
+    "thermo_source_hub_config_from_json",
+    "thermo_source_hub_config_from_str",
+    "thermo_source_hub_config_from_yaml",
     "UnitAvailabilityFn",
     "UnitConversionFn",
 ]
